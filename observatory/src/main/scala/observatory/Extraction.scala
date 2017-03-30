d2b834ec8b65b4c25942f428146a38e9882c4afd
Dragan Glumac <dragan.glumac@gmail.com>
2017-03-30 10:54:13 +0100

    RDD action

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -1,15 +1,27 @@
 package observatory
 
+import java.net.URL
+import java.nio.file.Paths
 import java.time.LocalDate
 
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-
-import scala.tools.nsc.interpreter.InputStream
+import sun.jvm.hotspot.opto.CallStaticJavaNode
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  type StationID = (String, String)
+
+  case class Station(stationId: StationID, latitude: Double, longitude: Double)
+
+  case class TemperatureRecord(stationId: StationID, month: Int, day: Int, fahrenheit: Double)
+
+
+  @transient lazy val conf: SparkConf = new SparkConf().setMaster("local").setAppName("Capstone")
+  @transient lazy val sc: SparkContext = new SparkContext(conf)
 
   /**
     * @param year             Year number
@@ -18,10 +30,18 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
-    val stations = getClass.getClassLoader.getResourceAsStream(stationsFile)
-    val temperatures = getClass.getClassLoader.getResourceAsStream(temperaturesFile)
-//    if (stations == null || temperatures == null)
-    Iterable.empty[(LocalDate, Location, Double)]
+    val stationsURL = getClass.getClassLoader.getResource(stationsFile)
+    val temperaturesURL = getClass.getClassLoader.getResource(temperaturesFile)
+
+    if (stationsURL == null || temperaturesURL == null)
+      Iterable.empty[(LocalDate, Location, Double)]
+    else {
+      val statRdd = stations(sc.textFile(fsPath(stationsURL)))
+      val tempRdd = temperatures(sc.textFile(fsPath(temperaturesURL)))
+
+      ???
+    }
+
   }
 
   /**
@@ -31,5 +51,28 @@ object Extraction {
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Double)]): Iterable[(Location, Double)] = {
     ???
   }
+
+  def fsPath(pathURL: URL): String = Paths.get(pathURL.toURI).toString
+
+  def stations(rawStations: RDD[String]): RDD[(StationID, Iterable[Station])] = {
+    def invalidStation(record: Array[String]) = {
+      val (stn, wban, lat, long) = (record(0), record(1), record(2), record(3))
+      (stn.isEmpty && wban.isEmpty) || lat.isEmpty || long.isEmpty
+    }
+
+    rawStations
+    .map(line => line.split(","))
+      .filter(stationRecord => !invalidStation(stationRecord))
+      .map(s => Station((s(0), s(1)), s(2).toDouble, s(3).toDouble))
+      .groupBy(_.stationId)
+  }
+
+  def temperatures(rawTemperatures: RDD[String]): RDD[(StationID, Iterable[TemperatureRecord])] =
+    rawTemperatures
+      .map(line => {
+        val t = line.split(",")
+        TemperatureRecord((t(0), t(1)), t(2).toInt, t(3).toInt, t(4).toDouble)
+      })
+      .groupBy(_.stationId)
 
 }
