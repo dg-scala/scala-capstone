797f1bc345376150596be91e1a46c1853b6eb127
Dragan Glumac <dragan.glumac@gmail.com>
2017-03-30 12:10:40 +0100

    Removed unused import

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -6,8 +6,7 @@ import java.time.LocalDate
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-import sun.jvm.hotspot.opto.CallStaticJavaNode
-
+  
 /**
   * 1st milestone: data extraction
   */
