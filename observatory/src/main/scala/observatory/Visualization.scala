f8973688596946e3c487efe7bceb86ee033ec813
Dragan Glumac <dragan.glumac@gmail.com>
2017-04-07 18:05:17 +0100

    interpolateColor implemented but not tested.

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -24,7 +24,7 @@ object Visualization {
 
         if (e != 9999.9) acc
         else if (haversineDistance(l, location) == 0) (n, d, t) // known temperature
-        else (n + weight(l, location, fudgeFactor) * t, d + weight(l, location, fudgeFactor), e)
+        else (n + weight(l, location, powerForWeights) * t, d + weight(l, location, powerForWeights), e)
       })
 
     if (exact != 9999.9) exact
@@ -37,7 +37,34 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    def closestPoints: ((Double, Color), (Double, Color)) = {
+      val pointsMap = points.toMap
+      val (low, high) =
+        pointsMap.keys.foldLeft((Double.MinValue, Double.MaxValue))((lh, pt) => {
+          val (l, h) = (lh._1, lh._2)
+
+          if (l == value && h == value) (l, h)
+          else if (pt == value) (pt, pt)
+          else if (pt < value) {
+            if (pt > l) (pt, h)
+            else (l, h)
+          }
+          else {
+            if (pt < h) (l, pt)
+            else (l, h)
+          }
+        })
+
+      ((low, pointsMap(low)), (high, pointsMap(high)))
+    }
+
+    val (before, after) = (closestPoints._1._1, closestPoints._2._1)
+    val (bCol, aCol) = (closestPoints._1._2, closestPoints._2._2)
+
+    if (before == value && value == after) bCol // exact point
+    else if (value < before) bCol // the lower bound
+    else if (value > after) aCol // the upper bound
+    else interpolate2(closestPoints._1, closestPoints._2, value)
   }
 
   /**
@@ -50,7 +77,7 @@ object Visualization {
   }
 
   // the higher the fudgeFactor the more closer geolocations dominate predictTemperatures calculation
-  val fudgeFactor = 2.0
+  val powerForWeights = 2.0
 
   def weight(l1: Location, l2: Location, power: Double): Double = 1 / pow(haversineDistance(l1, l2), power)
 
@@ -66,6 +93,17 @@ object Visualization {
     val a = pow(sin(dLat / 2), 2) + pow(sin(dLon / 2), 2) * cos(lat1.toRadians) * cos(lat2.toRadians)
     val c = 2 * asin(sqrt(a))
     earthRadius * c
+  }
+
+  def interpolate2(before: (Double, Color), after: (Double, Color), value: Double): Color = {
+    def interpolateComponent(y0: Int, y1: Int): Int =
+      (y0 + (value - before._1) * (y1 - y0) / (after._1 - before._1)).toInt
+
+    Color(
+      interpolateComponent(before._2.red, after._2.red),
+      interpolateComponent(before._2.green, after._2.green),
+      interpolateComponent(before._2.blue, after._2.blue)
+    )
   }
 
 }
