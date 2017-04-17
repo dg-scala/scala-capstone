9cb03337787e169300641e87ddade938a22e917e
Dragan Glumac <dragan.glumac@gmail.com>
2017-04-17 15:57:16 +0100

    Grader test for  passing, and added a test of my own plus a skeleton with commented pseudo-code for the  method implementation approach.

--- observatory/src/test/scala/observatory/Visualization2Test.scala
+++ observatory/src/test/scala/observatory/Visualization2Test.scala
@@ -7,13 +7,18 @@ import org.scalatest.prop.Checkers
 
 @RunWith(classOf[JUnitRunner])
 class Visualization2Test extends FunSuite with Checkers {
+  import Visualization2._
 
   test("bilinear interpolation") {
     val d00 = -1.0
     val d01 = 50.0
     val d10 = 49.07195176227695
     val d11 = 0.0
-
+    assert(bilinearInterpolation(0.1, 0.1, d00, d01, d10, d11) == 8.106475658604927)
+    assert(bilinearInterpolation(0, 0, d00, d01, d10, d11) == d00)
+    assert(bilinearInterpolation(0, 1, d00, d01, d10, d11) == d01)
+    assert(bilinearInterpolation(1, 0, d00, d01, d10, d11) == d10)
+    assert(bilinearInterpolation(1, 1, d00, d01, d10, d11) == d11)
   }
 
 }
