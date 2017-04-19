0db431a665a070819fe9c3f45faff672fe52c2c9
Dragan Glumac <dragan.glumac@gmail.com>
2017-04-19 14:49:18 +0100

    Creating directories on the fly now.

--- observatory/src/main/scala/observatory/package.scala
+++ observatory/src/main/scala/observatory/package.scala
@@ -3,5 +3,14 @@ package object observatory {
 
   def homeFile(relativePath: String): java.io.File = new java.io.File(s"${sys.env("HOME")}/$relativePath")
 
-  def relativeFile(relativePath: String): java.io.File = new java.io.File(relativePath)
+  def relativeFile(relativePath: String): java.io.File = {
+    val parts = relativePath.split("/")
+    val folderPath = parts.take(parts.size - 1).fold("")((f1, f2) =>
+      if (f1 == "") f2
+      else f1 + "/" + f2)
+    val folder = new java.io.File(folderPath)
+    folder.mkdirs
+
+    new java.io.File(relativePath)
+  }
 }
