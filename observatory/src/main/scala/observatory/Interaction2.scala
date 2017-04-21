c429a7e4a17064b0d83b4f6a338fb92b59769b40
Dragan Glumac <dragan.glumac@gmail.com>
2017-04-21 14:55:35 +0100

    Layers defined.

--- observatory/src/main/scala/observatory/Interaction2.scala
+++ observatory/src/main/scala/observatory/Interaction2.scala
@@ -1,15 +1,39 @@
 package observatory
 
+import observatory.LayerName.{Deviations, Temperatures}
+
 /**
   * 6th (and last) milestone: user interface polishing
   */
 object Interaction2 {
 
+  val temperatureColors = Seq(
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  )
+
+  val deviationColors = Seq(
+    (7.0, Color(0, 0, 0)),
+    (4.0, Color(255, 0, 0)),
+    (2.0, Color(255, 255, 0)),
+    (0.0, Color(255, 255, 255)),
+    (-2.0, Color(0, 255, 255)),
+    (-7.0, Color(0, 0, 255))
+  )
   /**
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    Seq(
+      Layer(Temperatures, temperatureColors, 1975 to 2015),
+      Layer(Deviations, deviationColors, 1990 to 2015)
+    )
   }
 
   /**
